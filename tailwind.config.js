61af0f8bfaf668cf6914388be5ad4aabcf61f313
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-08 15:44:33 +0700

    setup font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['PT Sans', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
