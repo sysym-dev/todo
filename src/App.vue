6323e1ca6e14b54b31a575a9f95b63417507bf7f
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-09 10:15:40 +0700

    card card

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="h-screen bg-gray-100 flex items-center justify-center">
+    <div class="bg-white p-5 rounded-lg space-y-4">
+      <h2 class="font-bold text-xl text-gray-900">Cards</h2>
+      <div class="grid grid-cols-4 gap-x-4">
+        <div class="border border-gray-200 text-gray-900 p-4 rounded-lg">
+          <p class="font-bold text-lg">BCA</p>
+          <p>Rp 1.680.000,00</p>
+        </div>
+        <div class="border border-gray-200 text-gray-900 p-4 rounded-lg">
+          <p class="font-bold text-lg">Dana</p>
+          <p>Rp 4.200,00</p>
+        </div>
+        <div class="border border-gray-200 text-gray-900 p-4 rounded-lg">
+          <p class="font-bold text-lg">Dompet</p>
+          <p>Rp 174.000,00</p>
+        </div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
