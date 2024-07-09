919fcca998eabbcecf75954b4ab789b6f0b0b9f9
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-10 05:59:43 +0700

    summary card

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,37 @@ import BaseBadge from 'src/components/base/base-badge.vue';
 </script>
 
 <template>
-  <div class="h-screen bg-gray-100 flex flex-col items-center justify-center">
+  <div class="min-h-screen bg-gray-100 flex flex-col items-center py-16">
     <div class="space-y-4 max-w-3xl w-full">
+      <base-card title="Summary">
+        <div class="grid grid-cols-3 gap-4">
+          <div>
+            <p class="font-bold text-xl text-gray-900">1.945.000,00</p>
+            <p class="text-sm text-gray-600">Balance</p>
+          </div>
+          <div>
+            <p class="font-bold text-xl text-gray-900">2.500.000,00</p>
+            <p class="text-sm text-gray-600">Income</p>
+          </div>
+          <div>
+            <p class="font-bold text-xl text-gray-900">645.000,00</p>
+            <p class="text-sm text-gray-600">Expense</p>
+          </div>
+        </div>
+      </base-card>
       <base-card title="Cards">
-        <div class="grid grid-cols-4 gap-x-4">
+        <div class="grid grid-cols-4 gap-4">
           <div class="border border-gray-200 text-gray-900 p-4 rounded-lg">
             <p class="font-bold text-lg">BCA</p>
-            <p>Rp 1.680.000,00</p>
+            <p>1.680.000,00</p>
           </div>
           <div class="border border-gray-200 text-gray-900 p-4 rounded-lg">
             <p class="font-bold text-lg">Dana</p>
-            <p>Rp 4.200,00</p>
+            <p>4.200,00</p>
           </div>
           <div class="border border-gray-200 text-gray-900 p-4 rounded-lg">
             <p class="font-bold text-lg">Dompet</p>
-            <p>Rp 174.000,00</p>
+            <p>174.000,00</p>
           </div>
         </div>
       </base-card>
@@ -39,28 +55,28 @@ import BaseBadge from 'src/components/base/base-badge.vue';
               <td class="border-b text-gray-900 py-2 px-3">
                 <base-badge color="red">Expense</base-badge>
               </td>
-              <td class="border-b text-gray-900 py-2 px-3">40000</td>
+              <td class="border-b text-gray-900 py-2 px-3">40.000,00</td>
             </tr>
             <tr>
               <td class="border-b text-gray-900 py-2 px-3">2024/07/09</td>
               <td class="border-b text-gray-900 py-2 px-3">
                 <base-badge color="red">Expense</base-badge>
               </td>
-              <td class="border-b text-gray-900 py-2 px-3">14000</td>
+              <td class="border-b text-gray-900 py-2 px-3">14.000,00</td>
             </tr>
             <tr>
               <td class="border-b text-gray-900 py-2 px-3">2024/07/09</td>
               <td class="border-b text-gray-900 py-2 px-3">
                 <base-badge color="red">Expense</base-badge>
               </td>
-              <td class="border-b text-gray-900 py-2 px-3">14000</td>
+              <td class="border-b text-gray-900 py-2 px-3">14.000,00</td>
             </tr>
             <tr>
               <td class="text-gray-900 py-2 px-3">2024/07/09</td>
               <td class="border-b text-gray-900 py-2 px-3">
                 <base-badge color="red">Expense</base-badge>
               </td>
-              <td class="text-gray-900 py-2 px-3">46000</td>
+              <td class="text-gray-900 py-2 px-3">46.000,00</td>
             </tr>
           </tbody>
         </table>
