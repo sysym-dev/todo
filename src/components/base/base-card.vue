bbe7c47adb4ba93bb0a78182beb55ab81ee86d15
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-11 18:09:52 +0700

    base card loading and error

--- src/components/base/base-card.vue
+++ src/components/base/base-card.vue
@@ -1,15 +1,31 @@
 <script setup>
+import BaseSpinner from 'src/components/base/base-spinner.vue';
+import BaseAlert from 'src/components/base/base-alert.vue';
+
 defineProps({
   title: String,
+  titleLoading: Boolean,
+  withContent: {
+    type: Boolean,
+    default: true,
+  },
+  error: Boolean,
+  errorMessage: String,
 });
 </script>
 
 <template>
   <div class="bg-white p-5 rounded-lg space-y-4">
     <div class="flex items-center justify-between">
-      <h2 class="font-bold text-xl text-gray-900">{{ title }}</h2>
+      <div class="flex items-center gap-x-2">
+        <h2 class="font-bold text-xl text-gray-900">{{ title }}</h2>
+        <base-spinner v-if="titleLoading" />
+      </div>
       <slot name="action" />
     </div>
-    <slot />
+    <base-alert v-if="error">
+      {{ errorMessage }}
+    </base-alert>
+    <slot v-else-if="withContent" />
   </div>
 </template>
