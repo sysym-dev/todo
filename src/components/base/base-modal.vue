29efc3b566e2b60951bf866760427706ee44b453
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-11 16:18:19 +0700

    base modal transition

--- src/components/base/base-modal.vue
+++ src/components/base/base-modal.vue
@@ -11,8 +11,18 @@ function onClickOutside() {
     v-if="visible"
     class="fixed top-0 left-0 right-0 bottom-0 bg-gray-400 bg-opacity-50 flex justify-center items-start py-16"
   >
-    <div class="max-w-md w-full" v-click-outside="onClickOutside">
-      <slot />
-    </div>
+    <transition
+      enter-active-class="duration-300 ease-out"
+      enter-from-class="opacity-0 scale-95"
+      enter-to-class="opacity-100 scale-100"
+      leave-active-class="duration-200 ease-in"
+      leave-from-class="opacity-100 scale-100"
+      leave-to-class="opacity-0 scale-95"
+      appear
+    >
+      <div class="max-w-md w-full" v-click-outside="onClickOutside">
+        <slot />
+      </div>
+    </transition>
   </div>
 </template>
