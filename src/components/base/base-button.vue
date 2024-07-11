e50476068befe7883638260151db7df7667be6e5
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-11 11:47:41 +0700

    base button

---
+++ src/components/base/base-button.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  size: {
+    type: String,
+    default: 'md',
+  },
+  color: {
+    type: String,
+    default: 'blue',
+  },
+});
+const size = computed(() => {
+  return {
+    md: 'text-base px-4 h-10 rounded-lg',
+    sm: 'text-sm px-2.5 h-7 rounded-md',
+    square: 'px-1 py-1 rounded-lg',
+  }[props.size];
+});
+const color = computed(() => {
+  return {
+    blue: 'bg-blue-600 text-white hover:bg-blue-500 active:bg-blue-400',
+    transparent: 'text-gray-600 hover:bg-gray-100 focus:bg-gray-100',
+  }[props.color];
+});
+</script>
+
+<template>
+  <button :class="['inline-flex items-center font-bold', size, color]">
+    <slot />
+  </button>
+</template>
