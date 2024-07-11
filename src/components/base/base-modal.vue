f1a89c5c6ef421d551dbda8dc237dcff1a9f0ee9
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-11 11:53:53 +0700

    base modal

---
+++ src/components/base/base-modal.vue
@@ -0,0 +1,14 @@
+<script setup>
+const visible = defineModel();
+</script>
+
+<template>
+  <div
+    v-if="visible"
+    class="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex justify-center items-start py-16"
+  >
+    <div class="max-w-md w-full">
+      <slot />
+    </div>
+  </div>
+</template>
