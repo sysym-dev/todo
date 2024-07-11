226a3b5b50e53f4d569848bb9731c6fb91b49c76
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-11 14:34:24 +0700

    base modal close on click outside

--- src/components/base/base-modal.vue
+++ src/components/base/base-modal.vue
@@ -1,13 +1,17 @@
 <script setup>
 const visible = defineModel();
+
+function onClickOutside() {
+  visible.value = false;
+}
 </script>
 
 <template>
   <div
     v-if="visible"
-    class="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex justify-center items-start py-16"
+    class="fixed top-0 left-0 right-0 bottom-0 bg-gray-400 bg-opacity-50 flex justify-center items-start py-16"
   >
-    <div class="max-w-md w-full">
+    <div class="max-w-md w-full" v-click-outside="onClickOutside">
       <slot />
     </div>
   </div>
