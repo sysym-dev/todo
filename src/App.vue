ff540e7f05ad592376f86d0550d8c1a7a7037d6f
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-11 21:09:55 +0700

    fetch cards

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,10 @@ import BaseFormItem from 'src/components/base/base-form-item.vue';
 import { X as CloseIcon } from '@vicons/tabler';
 import { ref } from 'vue';
 import SummaryCard from 'src/features/summary/components/summary-card.vue';
+import CardListCard from 'src/features/card/components/card-list-card.vue';
 
-const createCardModalVisible = ref(false);
 const createTransactionModalVisible = ref(false);
 
-function onOpenCreateCardModal() {
-  createCardModalVisible.value = true;
-}
-function onCloseCreateCardModal() {
-  createCardModalVisible.value = false;
-}
 function onOpenCreateTransactionModal() {
   createTransactionModalVisible.value = true;
 }
@@ -31,28 +25,7 @@ function onCloseCreateTransactionModal() {
   <div class="min-h-screen bg-gray-100 flex flex-col items-center py-16">
     <div class="space-y-4 max-w-3xl w-full">
       <summary-card />
-      <base-card title="Cards">
-        <template #action>
-          <base-button size="sm" @click="onOpenCreateCardModal"
-            >New Card</base-button
-          >
-        </template>
-
-        <div class="grid grid-cols-4 gap-4">
-          <div class="border border-gray-200 text-gray-900 p-4 rounded-lg">
-            <p class="font-bold text-lg">BCA</p>
-            <p>1.680.000,00</p>
-          </div>
-          <div class="border border-gray-200 text-gray-900 p-4 rounded-lg">
-            <p class="font-bold text-lg">Dana</p>
-            <p>4.200,00</p>
-          </div>
-          <div class="border border-gray-200 text-gray-900 p-4 rounded-lg">
-            <p class="font-bold text-lg">Dompet</p>
-            <p>174.000,00</p>
-          </div>
-        </div>
-      </base-card>
+      <card-list-card />
       <base-card title="Transactions">
         <template #action>
           <base-button size="sm" @click="onOpenCreateTransactionModal"
@@ -103,31 +76,6 @@ function onCloseCreateTransactionModal() {
         </table>
       </base-card>
     </div>
-
-    <base-modal v-model="createCardModalVisible">
-      <base-card title="New Card">
-        <template #action>
-          <base-button
-            size="square"
-            color="transparent"
-            @click="onCloseCreateCardModal"
-          >
-            <close-icon class="w-4 h-4" />
-          </base-button>
-        </template>
-
-        <base-form-item label="Name">
-          <base-input id="name" placeholder="Name" />
-        </base-form-item>
-
-        <div class="space-x-2">
-          <base-button> Save </base-button>
-          <base-button color="transparent" @click="onCloseCreateCardModal">
-            Cancel
-          </base-button>
-        </div>
-      </base-card>
-    </base-modal>
 
     <base-modal v-model="createTransactionModalVisible">
       <base-card title="New Transaction">
