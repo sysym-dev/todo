17403fe7f216d1e13332e5147536f46e7d7deb2b
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-11 16:32:47 +0700

    base input

---
+++ src/components/base/base-input.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  type: {
+    type: String,
+    default: 'text',
+  },
+  id: String,
+  placeholder: String,
+});
+</script>
+
+<template>
+  <input
+    :type="type"
+    :name="id"
+    :id="id"
+    class="block w-full border border-gray-200 rounded-lg h-10 placeholder-gray-300 hover:border-gray-300 text-gray-900 focus:border-blue-600 focus:outline-0"
+    :placeholder="placeholder"
+  />
+</template>
