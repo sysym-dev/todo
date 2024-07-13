8c05765979ec46741c5e49e1f111c411b3e3fcad
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-13 22:45:36 +0700

    emitter

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import { useClickOutside } from './plugins/click-outside';
+import { useEmitter } from './plugins/emitter';
 
 const app = createApp(App);
 
 useClickOutside(app);
+useEmitter(app);
 
 app.mount('#app');
