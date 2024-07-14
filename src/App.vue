97b15ed8db8b11498aa0d6e6f9cd77a2063590e8
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-14 14:08:00 +0700

    use router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <script setup>
-import SummaryCard from 'src/features/summary/components/summary-card.vue';
-import CardListCard from 'src/features/card/components/card-list-card.vue';
-import TransactionTableCard from 'src/features/transaction/components/transaction-table-card.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
 </script>
 
 <template>
-  <div class="min-h-screen bg-gray-100 flex flex-col items-center py-16">
-    <div class="space-y-4 max-w-3xl w-full">
-      <summary-card />
-      <card-list-card />
-      <transaction-table-card />
-    </div>
-  </div>
+  <component :is="route.meta.layout">
+    <router-view />
+  </component>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import './style.css';
 import App from './App.vue';
 import { useClickOutside } from './plugins/click-outside';
 import { useEmitter } from './plugins/emitter';
+import { useRouter } from './plugins/router';
+import { routes } from './routes';
+import { useLayout } from './plugins/layout';
 
 const app = createApp(App);
 
 useClickOutside(app);
 useEmitter(app);
+useRouter(app, routes);
+useLayout(app);
 
 app.mount('#app');
