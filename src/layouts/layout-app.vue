c9c9ff52637ecd992f575dd1f3b9d9236a090bc6
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-14 14:10:02 +0700

    layout app

---
+++ src/layouts/layout-app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen bg-gray-100 flex flex-col items-center py-16">
+    <div class="space-y-4 max-w-3xl w-full">
+      <slot />
+    </div>
+  </div>
+</template>
