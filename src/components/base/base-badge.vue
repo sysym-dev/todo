3c5a7924ea02abc28b45424db4d288adcaa78ff8
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-15 06:04:29 +0700

    base badge color and size

--- src/components/base/base-badge.vue
+++ src/components/base/base-badge.vue
@@ -6,18 +6,30 @@ const props = defineProps({
     type: String,
     default: 'light',
   },
+  size: {
+    type: String,
+    default: 'md',
+  },
 });
 
 const color = computed(() => {
   return {
     red: 'bg-red-100 text-red-600',
     green: 'bg-green-100 text-green-600',
+    blue: 'bg-blue-100 text-blue-600',
+    light: 'bg-gray-100 text-gray-600',
   }[props.color];
+});
+const size = computed(() => {
+  return {
+    md: 'px-1.5 py-0.5 rounded-md',
+    sm: 'px-1 h-4 rounded-md',
+  }[props.size];
 });
 </script>
 
 <template>
-  <div :class="['text-xs px-1.5 py-0.5 rounded-md inline-block', color]">
+  <div :class="['text-xs inline-block', color, size]">
     <slot />
   </div>
 </template>
