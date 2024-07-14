1febcb001d124b43f8a6382ddc26fdf4e65d325e
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-14 11:52:33 +0700

    parseDate util

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,5 +1,12 @@
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc);
 
 export function formatDate(date, format) {
   return dayjs(date).format(format);
+}
+
+export function parseDate(date) {
+  return dayjs(date);
 }
