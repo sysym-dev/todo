611bf3d5620c4cda4ffa2e95288ecc0928d673d3
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-15 06:04:40 +0700

    base button fullwidth

--- src/components/base/base-button.vue
+++ src/components/base/base-button.vue
@@ -21,13 +21,15 @@ const props = defineProps({
   },
   loading: Boolean,
   to: null,
+  fullwidth: Boolean,
 });
 const emit = defineEmits(['click']);
 
 const size = computed(() => {
   return {
-    md: 'text-base px-4 h-10 rounded-lg',
-    sm: 'text-sm px-2.5 h-7 rounded-md',
+    md: 'text-base px-4 h-10 rounded-lg gap-x-2',
+    sm: 'text-sm px-2.5 h-7 rounded-md gap-x-1',
+    'sm-square': 'text-sm w-7 h-7 rounded-md flex',
     square: 'px-1 py-1 rounded-lg',
   }[props.size];
 });
@@ -35,6 +37,8 @@ const color = computed(() => {
   return {
     blue: 'bg-blue-600 text-white hover:bg-blue-500 active:bg-blue-400 disabled:bg-blue-400',
     transparent: 'text-gray-600 hover:bg-gray-100 active:bg-gray-50',
+    'transparent-bordered':
+      'text-gray-600 hover:bg-gray-100 active:bg-gray-50 border border-gray-200',
     'transparent-red': 'text-red-600 hover:bg-red-100 active:bg-red-50',
   }[props.color];
 });
@@ -49,7 +53,12 @@ function onClick() {
     :to="to"
     :is="tag"
     :type="type"
-    :class="['inline-flex items-center font-bold gap-x-2', size, color]"
+    :class="[
+      'inline-flex items-center justify-center font-bold',
+      size,
+      color,
+      fullwidth ? 'w-full' : '',
+    ]"
     :disabled="loading"
     @click="onClick"
   >
