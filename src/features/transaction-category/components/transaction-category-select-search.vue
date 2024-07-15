87d7be3388a7adbb922287fda1b4e19785f5dcf4
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-16 06:57:39 +0700

    transaction category select search create

--- src/features/transaction-category/components/transaction-category-select-search.vue
+++ src/features/transaction-category/components/transaction-category-select-search.vue
@@ -1,26 +1,31 @@
 <script setup>
 import BaseSelectSearch from 'src/components/base/base-select-search.vue';
 import { useRequest } from 'src/cores/request/request';
-import { reactive } from 'vue';
+import { reactive, computed, ref } from 'vue';
 
 const {
-  loading,
-  request,
+  loading: loadingFetch,
+  request: fetchTransactionCategories,
   data: transactionCategories,
 } = useRequest('/api/transaction-categories', {
   initData: {
     data: [],
   },
 });
+const { loading: loadingCreate, request: createTransactionCategory } =
+  useRequest('/api/transaction-categories');
 
 const selected = defineModel();
+
+const visible = ref(false);
+const loading = computed(() => loadingFetch.value || loadingCreate.value);
 const params = reactive({
   limit: 10,
   search: null,
 });
 
-function loadCards() {
-  request({
+function loadTransactionCategories() {
+  fetchTransactionCategories({
     params,
   });
 }
@@ -28,18 +33,31 @@ function loadCards() {
 function onFocused() {
   params.limit = 10;
 
-  loadCards();
+  loadTransactionCategories();
 }
 function onSearch() {
   params.limit = 10;
 
-  loadCards();
+  loadTransactionCategories();
 }
 function onLoadMore() {
   if (params.limit < transactionCategories.value.meta.total) {
     params.limit += 5;
 
-    loadCards();
+    loadTransactionCategories();
+  }
+}
+async function onCreate(name) {
+  const res = await createTransactionCategory({
+    method: 'post',
+    data: {
+      name,
+    },
+  });
+
+  if (res.success) {
+    selected.value = res.data;
+    visible.value = false;
   }
 }
 </script>
@@ -49,10 +67,13 @@ function onLoadMore() {
     placeholder="Select Category"
     :options="transactionCategories.data"
     :loading="loading"
+    with-create
     v-model="selected"
+    v-model:visible="visible"
     v-model:search="params.search"
     @focused="onFocused"
     @search="onSearch"
     @end-scroll="onLoadMore"
+    @create="onCreate"
   />
 </template>
