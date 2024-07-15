1bf562e8b076b064ee2c5b7b078fba241c233baa
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-15 21:36:33 +0700

    base description list

---
+++ src/components/base/base-description-list.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  columns: Array,
+  data: Object,
+});
+</script>
+
+<template>
+  <dl class="gap-4 grid grid-cols-2">
+    <div v-for="column in columns" :key="column.id">
+      <dt class="font-bold text-gray-900">{{ column.name }}</dt>
+      <dd class="text-gray-900">
+        <component v-if="column.render" :is="column.render" :item="data" />
+        <template v-else>
+          {{ column.value(data) }}
+        </template>
+      </dd>
+    </div>
+  </dl>
+</template>
