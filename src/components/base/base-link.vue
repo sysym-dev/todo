3d5b9106b5d01f24d0b6bdc7aed4dc1193d1491a
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-16 06:57:06 +0700

    base link

---
+++ src/components/base/base-link.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  href: String,
+});
+const emit = defineEmits(['click']);
+
+function onClick() {
+  emit('click');
+}
+</script>
+
+<template>
+  <a :href="href" class="text-blue-600" @click="onClick"><slot /></a>
+</template>
