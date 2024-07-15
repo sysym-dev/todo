fc3fa4d5016afa36b96a00b3da3a88daca4fffc6
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-15 09:25:01 +0700

    transaction index page

---
+++ src/features/transaction/pages/transaction-index-page.vue
@@ -0,0 +1,7 @@
+<script setup>
+import TransactionTableCard from 'src/features/transaction/components/transaction-table-card.vue';
+</script>
+
+<template>
+  <transaction-table-card />
+</template>
