e9c560866b8eda622e52c36a74030e96f06ffabf
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-16 06:44:53 +0700

    base modal scroll

--- src/components/base/base-modal.vue
+++ src/components/base/base-modal.vue
@@ -10,7 +10,10 @@ function onClickOutside() {
 
 watch(visible, (value) => {
   if (value) {
+    document.body.classList.add('overflow-hidden');
     emit('opened');
+  } else {
+    document.body.classList.remove('overflow-hidden');
   }
 });
 </script>
@@ -18,7 +21,7 @@ watch(visible, (value) => {
 <template>
   <div
     v-if="visible"
-    class="fixed z-10 top-0 left-0 right-0 bottom-0 bg-gray-400 bg-opacity-50 flex justify-center items-start py-16"
+    class="fixed z-10 top-0 left-0 w-full h-screen overflow-y-scroll bg-gray-400 bg-opacity-50 flex justify-center items-start py-16"
   >
     <transition
       enter-active-class="duration-300 ease-out"
