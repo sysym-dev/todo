0a526af76c7dbab0208930e1cc75bde4d6467745
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-16 11:56:29 +0700

    transaction detail modal find card and category

--- src/features/transaction/components/transaction-detail-modal.vue
+++ src/features/transaction/components/transaction-detail-modal.vue
@@ -43,6 +43,17 @@ const columns = [
     value: (item) => formatCurrency(item.amount),
   },
   {
+    key: 'card',
+    name: 'Card',
+    value: (item) => item.card.name,
+  },
+  {
+    key: 'category',
+    name: 'Category',
+    value: (item) =>
+      item.transactionCategory ? item.transactionCategory.name : '-',
+  },
+  {
     id: 'description',
     name: 'Description',
     class: 'col-span-2',
@@ -56,6 +67,9 @@ function onClose() {
 function onOpened() {
   request({
     url: `/api/transactions/${props.transactionId}`,
+    params: {
+      include: ['card', 'transaction_category'],
+    },
   });
 }
 </script>
