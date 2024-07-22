6f8f4c48a3068c639758906f8cae2dec724b421c
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-22 17:46:09 +0700

    store plugin

--- src/main.js
+++ src/main.js
@@ -6,12 +6,14 @@ import { useEmitter } from './plugins/emitter';
 import { useRouter } from './plugins/router';
 import { routes } from './routes';
 import { useLayout } from './plugins/layout';
+import { useStore } from './plugins/store';
 
 const app = createApp(App);
 
 useClickOutside(app);
 useEmitter(app);
 useRouter(app, routes);
+useStore(app);
 useLayout(app);
 
 app.mount('#app');
