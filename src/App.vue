8d0457664aeaf3fc74ec2c7a99d75fa6cbf31fb4
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-25 21:59:04 +0700

    app toast

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useRoute } from 'vue-router';
 import { useAuthStore } from './features/auth/auth.store';
+import AppToast from './components/app/app-toast.vue';
 
 const authStore = useAuthStore();
 
@@ -10,6 +11,7 @@ authStore.loadMe();
 </script>
 
 <template>
+  <app-toast />
   <component :is="route.meta.layout">
     <router-view />
   </component>
