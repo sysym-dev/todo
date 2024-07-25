9ea7456de2ad61cb35f5d9ac517d7d1302fc00e1
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-25 21:28:17 +0700

    login with google get token

--- src/features/auth/pages/auth-login-page.vue
+++ src/features/auth/pages/auth-login-page.vue
@@ -1,17 +1,34 @@
 <script setup>
 import BaseCard from 'src/components/base/base-card.vue';
 import BaseButton from 'src/components/base/base-button.vue';
+import BaseAlert from 'src/components/base/base-alert.vue';
 import {
   BrandGoogle as GoogleIcon,
   BrandGithub as GithubIcon,
 } from '@vicons/tabler';
+import { googleTokenLogin } from 'vue3-google-login';
+
+async function onLoginGoogle() {
+  const res = await googleTokenLogin();
+
+  // res.access_token
+}
 </script>
 
 <template>
   <div class="max-w-sm mx-auto w-full">
     <base-card title="Login">
       <p class="text-gray-600">Login with your social account</p>
-      <base-button color="transparent-bordered" fullwidth>
+      <base-alert v-if="false">
+        {{ 'errorMessage' }}
+      </base-alert>
+      <base-button
+        :loading="false"
+        loading-block
+        color="transparent-bordered"
+        fullwidth
+        @click="onLoginGoogle"
+      >
         <google-icon class="w-4 h-4" />
         Login With Google
       </base-button>
