e5b6ba143ba5df838d8a6deda3f4d4156bfaccbf
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-29 08:39:20 +0700

    auth store login action

--- src/features/auth/auth.store.js
+++ src/features/auth/auth.store.js
@@ -2,18 +2,30 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 import { useRequest } from 'src/cores/request/request';
 
-export const useAuthStore = defineStore('auth', () => {
-  const { loading, error, request: fetchMe } = useRequest('/api/me');
+export const useAuthStore = defineStore(
+  'auth',
+  () => {
+    const { loading, error, request: fetchMe } = useRequest('/api/me');
 
-  const me = ref(null);
+    const loggedIn = ref(false);
+    const accessToken = ref(null);
+    const me = ref(null);
 
-  async function loadMe() {
-    const res = await fetchMe();
+    async function loadMe() {
+      const res = await fetchMe();
 
-    if (res.success) {
-      me.value = res.data;
+      if (res.success) {
+        me.value = res.data;
+      }
     }
-  }
 
-  return { loading, error, me, loadMe };
-});
+    async function login(data) {
+      accessToken.value = data.accessToken;
+      me.value = data.me;
+      loggedIn.value = true;
+    }
+
+    return { loading, error, me, accessToken, loggedIn, loadMe, login };
+  },
+  { persist: true },
+);
