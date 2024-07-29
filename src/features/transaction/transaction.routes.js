c93ba7389c93b011bb5b813594d60138783065df
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-29 21:35:30 +0700

    protect app page require auth

--- src/features/transaction/transaction.routes.js
+++ src/features/transaction/transaction.routes.js
@@ -4,6 +4,7 @@ export const transactionRoutes = [
     name: 'transaction.index',
     meta: {
       layout: 'LayoutApp',
+      requireAuth: true,
     },
     component: () => import('./pages/transaction-index-page.vue'),
   },
