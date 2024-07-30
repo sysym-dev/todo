7034d14a2fdd7dc3d4522ff73a2aaddd05cffcb2
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-30 11:39:25 +0700

    logout on load me error

--- src/features/auth/auth.store.js
+++ src/features/auth/auth.store.js
@@ -1,11 +1,18 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 import { useRequest } from 'src/cores/request/request';
+import { useRouter } from 'vue-router';
 
 export const useAuthStore = defineStore(
   'auth',
   () => {
-    const { loading, error, request: fetchMe } = useRequest('/api/me');
+    const router = useRouter();
+    const {
+      loading,
+      error,
+      request: fetchMe,
+      resetError,
+    } = useRequest('/api/me');
 
     const loggedIn = ref(false);
     const accessToken = ref(null);
@@ -16,13 +23,24 @@ export const useAuthStore = defineStore(
 
       if (res.success) {
         me.value = res.data;
+      } else {
+        logout();
+        router.push({ name: 'auth.login' });
       }
     }
 
     async function login(data) {
+      resetError();
+
       accessToken.value = data.accessToken;
       me.value = data.me;
       loggedIn.value = true;
+    }
+
+    function logout() {
+      loggedIn.value = false;
+      accessToken.value = null;
+      me.value = null;
     }
 
     return { loading, error, me, accessToken, loggedIn, loadMe, login };
