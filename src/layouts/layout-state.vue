0ea115627fc16e42491c72b7c2d6b30af6e1b6ed
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-08-11 15:16:53 +0700

    layout route

---
+++ src/layouts/layout-state.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="bg-white w-full h-screen flex flex-col justify-center items-center"
+  >
+    <slot />
+  </div>
+</template>
--- src/layouts/layouts.js
+++ src/layouts/layouts.js
@@ -1,7 +1,9 @@
 import LayoutApp from './layout-app.vue';
 import LayoutAuth from './layout-auth.vue';
+import LayoutState from './layout-state.vue';
 
 export const layouts = {
   LayoutApp,
   LayoutAuth,
+  LayoutState,
 };
