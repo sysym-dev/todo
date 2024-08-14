a42bb071d09680c05103ac73d6de55f6f969848a
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-08-14 21:47:51 +0700

    import input card

--- src/features/transaction/components/transaction-import-modal.vue
+++ src/features/transaction/components/transaction-import-modal.vue
@@ -4,6 +4,7 @@ import BaseButton from 'src/components/base/base-button.vue';
 import BaseModal from 'src/components/base/base-modal.vue';
 import BaseInputFile from 'src/components/base/base-input-file.vue';
 import BaseFormItem from 'src/components/base/base-form-item.vue';
+import CardSelectSearch from 'src/features/card/components/card-select-search.vue';
 import { X as CloseIcon } from '@vicons/tabler';
 import { reactive, inject } from 'vue';
 import { useRequest } from 'src/cores/request/request';
@@ -27,11 +28,18 @@ const {
 } = useValidation(
   z.object({
     file: z.any(),
+    card_id: z
+      .number({
+        invalid_type_error: 'card is required',
+        required_error: 'card is required',
+      })
+      .positive({ message: 'card is required' }),
   }),
 );
 
 const form = reactive({
   file: null,
+  card: null,
 });
 
 const visible = defineModel();
@@ -44,14 +52,23 @@ function onOpened() {
   resetErrorRequest();
 
   form.file = null;
+  form.card = null;
 }
 async function onSubmit() {
-  const validation = await validate(form);
+  const validation = await validate({
+    file: form.file,
+    card_id: form.card ? form.card.id : null,
+  });
 
   if (validation.success) {
+    const formData = new FormData();
+
+    formData.append('file', validation.data.file);
+    formData.append('card_id', validation.data.card_id);
+
     const res = await request({
       method: 'post',
-      data: validation.data,
+      data: formData,
     });
 
     if (res.success) {
@@ -80,6 +97,17 @@ async function onSubmit() {
       </template>
 
       <form class="space-y-4" @submit.prevent="onSubmit">
+        <base-form-item
+          label="Card"
+          :color="hasError('card_id') ? 'red' : 'default'"
+          :message="getError('card_id')"
+        >
+          <card-select-search
+            :color="hasError('card_id') ? 'red' : 'default'"
+            v-model="form.card"
+          />
+        </base-form-item>
+
         <base-form-item
           label="Upload File"
           :color="hasError('file') ? 'red' : 'default'"
