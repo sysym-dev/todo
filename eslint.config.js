d06f05d88ea7abb4396ed07abea248d1b9e62793
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-08-17 22:13:33 +0700

    setup linter

---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+import globals from 'globals';
+import pluginVue from 'eslint-plugin-vue';
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  ...pluginVue.configs['flat/essential'],
+];
