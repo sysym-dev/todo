fda00371613beba30a076c982b63abd1a2b6c4cd
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-08-19 21:37:40 +0700

    click outside

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import { useEmitter } from './plugins/emitter';
+import { useClickOutisde } from './plugins/click-outside';
 
 const app = createApp(App);
 
 useEmitter(app);
+useClickOutisde(app);
 
 app.mount('#app');
