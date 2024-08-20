3c59bc34a0a8844d500f634488cf6bba118b7a20
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-08-20 21:53:01 +0700

    store plugin

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import './style.css';
 import App from './App.vue';
 import { useEmitter } from './plugins/emitter';
 import { useClickOutisde } from './plugins/click-outside';
+import { useStore } from './plugins/store';
 
 const app = createApp(App);
 
 useEmitter(app);
 useClickOutisde(app);
+useStore(app);
 
 app.mount('#app');
