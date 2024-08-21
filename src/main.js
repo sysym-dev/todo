832915d655bb60d1e1e0e1181fe6eb176a4272de
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-08-21 07:20:49 +0700

    router plugin

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import App from './App.vue';
 import { useEmitter } from './plugins/emitter';
 import { useClickOutisde } from './plugins/click-outside';
 import { useStore } from './plugins/store';
+import { useRouter } from './plugins/router';
+import { routes } from './routes';
 
 const app = createApp(App);
 
 useEmitter(app);
 useClickOutisde(app);
+useRouter(app, routes);
 useStore(app);
 
 app.mount('#app');
