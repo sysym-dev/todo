3fe77b5edfb579db27a9f6e572013d9ff322c48f
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-08-21 21:23:59 +0700

    base heading

---
+++ src/components/base/base-heading.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  size: {
+    type: String,
+    default: 'text-xl',
+  },
+  title: String,
+});
+</script>
+
+<template>
+  <div class="flex items-center justify-between">
+    <h1 :class="['font-bold text-gray-900', size]">{{ title }}</h1>
+    <slot name="action" />
+  </div>
+</template>
