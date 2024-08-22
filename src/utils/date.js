833b3847825db7ce51d2bf9091754ea6be92f341
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-08-22 07:00:58 +0700

    date util relative time

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,7 +1,9 @@
 import dayjs from 'dayjs';
 import isBetween from 'dayjs/plugin/isBetween';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 dayjs.extend(isBetween);
+dayjs.extend(relativeTime);
 
 export function parseDate(date) {
   return dayjs(date);
