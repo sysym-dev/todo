7008d3c50f5dd424b753dc460066c92796ed4df4
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-08-25 21:14:26 +0700

    dynamic title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup>
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute();
+const router = useRouter();
+
+router.afterEach((to) => {
+  const appTitle = import.meta.env.VITE_APP_TITLE;
+  const pageTitle = to.meta.title;
+
+  document.title = `${appTitle} - ${pageTitle}`;
+});
 </script>
 
 <template>
