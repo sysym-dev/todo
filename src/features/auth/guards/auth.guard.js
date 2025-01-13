4479e67a0bf51516fc1633062ca04449fc338d9e
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-01-13 19:12:09 +0700

    feat: auth guard check session

--- src/features/auth/guards/auth.guard.js
+++ src/features/auth/guards/auth.guard.js
@@ -1,16 +1,17 @@
 import { useAuthStore } from 'src/features/auth/auth.store';
 
 export function useAuthGuard(router) {
-  router.beforeEach((to) => {
+  router.beforeEach(async (to) => {
     const authStore = useAuthStore();
 
-    if (
-      to.matched.some((route) => route.meta.requireAuth) &&
-      !authStore.loggedIn
-    ) {
-      return {
-        name: 'auth.login',
-      };
+    if (to.matched.some((route) => route.meta.requireAuth)) {
+      const loggedIn = await authStore.checkSession();
+
+      if (!loggedIn) {
+        return {
+          name: 'auth.login',
+        };
+      }
     }
   });
 }
