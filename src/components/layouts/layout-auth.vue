20ae27ca1b226c37f31dd8290821166c01833d5c
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-01-13 19:11:22 +0700

    feat: layout auth toast

--- src/components/layouts/layout-auth.vue
+++ src/components/layouts/layout-auth.vue
@@ -1,4 +1,9 @@
+<script setup>
+import AppToast from 'src/components/app/app-toast.vue';
+</script>
+
 <template>
+  <app-toast />
   <div class="h-screen flex items-center justify-center py-24">
     <div class="max-w-screen-sm mx-auto w-full px-4 md:px-0">
       <slot />
