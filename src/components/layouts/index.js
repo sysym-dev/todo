4bee66737cf23e68af8ad0de8845bbb7d62ed267
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-01-13 14:20:14 +0700

    feat: layout auth

--- src/components/layouts/index.js
+++ src/components/layouts/index.js
@@ -1,5 +1,7 @@
 import LayoutApp from './layout-app.vue';
+import LayoutAuth from './layout-auth.vue';
 
 export const layouts = {
   LayoutApp,
+  LayoutAuth,
 };
---
+++ src/components/layouts/layout-auth.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-screen flex items-center justify-center py-24">
+    <div class="max-w-screen-sm mx-auto w-full px-4 md:px-0">
+      <slot />
+    </div>
+  </div>
+</template>
