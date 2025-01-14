a88323cd0bfcbc28e7630d950761d780cc513be7
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-01-15 06:44:39 +0700

    feat: add user id payload to create todo

--- src/features/auth/auth.store.js
+++ src/features/auth/auth.store.js
@@ -4,13 +4,22 @@ import { inject, ref } from 'vue';
 export const useAuthStore = defineStore('auth', () => {
   const supabase = inject('supabase');
 
+  const user = ref(null);
+
   async function checkSession() {
     const session = await supabase.auth.getSession();
+
+    const loggedIn = !!session.data.session;
+
+    if (loggedIn && !user.value) {
+      user.value = await supabase.auth.getUser();
+    }
 
     return !!session.data.session;
   }
 
   return {
     checkSession,
+    user,
   };
 });
