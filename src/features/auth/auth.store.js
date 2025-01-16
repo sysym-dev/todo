55789fc9a9211f0c7ecf8e7d62a445dab015b4c0
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-01-16 14:22:16 +0700

    fix: login register set supabase user

--- src/features/auth/auth.store.js
+++ src/features/auth/auth.store.js
@@ -6,13 +6,16 @@ export const useAuthStore = defineStore('auth', () => {
 
   const user = ref(null);
 
+  async function login() {
+    user.value = await supabase.auth.getUser();
+  }
   async function checkSession() {
     const session = await supabase.auth.getSession();
 
     const loggedIn = !!session.data.session;
 
     if (loggedIn && !user.value) {
-      user.value = await supabase.auth.getUser();
+      await login();
     }
 
     return !!session.data.session;
@@ -20,6 +23,7 @@ export const useAuthStore = defineStore('auth', () => {
 
   return {
     checkSession,
+    login,
     user,
   };
 });
