daac7d8e06b6e0503f8d9e081a4c519c45570f3d
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-01-17 09:36:05 +0700

    feat: toast custom position

--- src/components/app/app-toast.vue
+++ src/components/app/app-toast.vue
@@ -2,6 +2,13 @@
 import { ref, inject } from 'vue';
 import BaseAlert from 'src/components/base/base-alert.vue';
 
+defineProps({
+  position: {
+    type: String,
+    default: 'top-center',
+  },
+});
+
 const emitter = inject('emitter');
 
 const toasts = ref([]);
@@ -25,7 +32,14 @@ emitter.on('create-toast', onCreateToast);
 </script>
 
 <template>
-  <div class="fixed top-4 left-1/2 -translate-x-1/2 space-y-4 z-20">
+  <div
+    :class="[
+      'fixed space-y-4 z-20',
+      position === 'top-center'
+        ? 'top-4 left-1/2 -translate-x-1/2'
+        : 'bottom-4 left-1/2 -translate-x-1/2',
+    ]"
+  >
     <base-alert
       v-for="toast in toasts"
       :key="toast.id"
