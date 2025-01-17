48760aedbc536c6b371d17d4dca4c41f57892594
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-01-17 09:26:37 +0700

    feat: app toast use base alert

--- src/components/app/app-toast.vue
+++ src/components/app/app-toast.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, inject } from 'vue';
-import { AlertTriangle as AlertIcon, X as CloseIcon } from '@vicons/tabler';
+import BaseAlert from 'src/components/base/base-alert.vue';
 
 const emitter = inject('emitter');
 
@@ -25,19 +25,11 @@ emitter.on('create-toast', onCreateToast);
 </script>
 
 <template>
-  <div class="fixed top-4 left-1/2 -translate-x-1/2 space-y-4">
-    <div
+  <div class="fixed top-4 left-1/2 -translate-x-1/2 space-y-4 z-20">
+    <base-alert
       v-for="toast in toasts"
       :key="toast.id"
-      class="border border-red-600 bg-red-50 text-red-600 px-3 py-2 flex items-center justify-between min-w-[400px]"
-    >
-      <div class="flex items-center gap-x-2">
-        <alert-icon class="w-4 h-4" />
-        <p>{{ toast.message }}</p>
-      </div>
-      <button v-if="false">
-        <close-icon class="w-4 h-4" />
-      </button>
-    </div>
+      :message="toast.message"
+    />
   </div>
 </template>
