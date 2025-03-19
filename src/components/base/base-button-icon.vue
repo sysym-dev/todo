1a790f9fe71b30aaecba99ab4fb1bb06fad25421
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-03-19 21:50:16 +0700

    feat: base button icon

---
+++ src/components/base/base-button-icon.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  icon: null,
+  padless: Boolean,
+});
+</script>
+
+<template>
+  <button
+    type="button"
+    :class="[
+      'text-gray-900 flex items-center justify-center',
+      padless ? '' : 'w-6 h-6',
+    ]"
+  >
+    <slot>
+      <component :is="icon" class="w-4 h-4" />
+    </slot>
+  </button>
+</template>
